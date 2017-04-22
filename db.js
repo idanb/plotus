1698816132c4d5b7b1e4a9d0c3921e484230e86a
idan belassen <idanbelah@gmail.com>
2017-04-23 01:14:49 +0300

    change db

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 
 
 var db = mysql.createConnection({
-    host     : 'plotus.000webhostapp.com', //'plotus.000webhostapp.com' '127.0.0.1'
-    user     : 'id1444985_plotus', //id1444985_plotus
-    password : '24118324', //'24118324'
-    database : 'plotus'
+    host     : 'sql11.freesqldatabase.com', //'plotus.000webhostapp.com' '127.0.0.1'
+    user     : 'sql11170577', //id1444985_plotus
+    password : 'wpehRVxDJb', //'24118324'
+    database : 'sql11170577' //'id1444985_plotus'
 });
 
 db.connect(function(err) {
