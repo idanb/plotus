529fa4d8fbd658db31a59e3ef7f97d610c7a9779
idan belassen <idanbelah@gmail.com>
2017-04-29 18:07:54 +0300

    update

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ var mysql = require('mysql');
 var db = mysql.createConnection({
     host     : 'us-cdbr-iron-east-03.cleardb.net',//'localhost',//'sql11.freesqldatabase.com', //'plotus.000webhostapp.com' '127.0.0.1'
     user     : 'bdd1f1f96bdbf6',//'root',//'sql11170577', //
-    password : 'wpehRVxDJb',//'2411',//'wpehRVxDJb', //
+    password : '8d1c7061',//'2411',//'wpehRVxDJb', //
     database : 'heroku_73428ab82ab4337'//'plotus'//'sql11170577' //'id1444985_plotus' //er
 });
 
