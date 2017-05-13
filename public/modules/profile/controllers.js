73056d13b3046094ab9082c311f7eac9703f2e84
idanb <idanbelah@gmail.com>
2017-05-13 11:01:05 +0300

    bug fixes and more

--- public/modules/profile/controllers.js
+++ public/modules/profile/controllers.js
@@ -5,7 +5,17 @@ angular.module('Profile')
             function ($scope, $rootScope, $cookies, $location, $http, $sce, $window, SessionFactory) {
                 if(typeof $cookies.getObject('globals') == 'undefined') $location.path('/login');
                 $scope.user = SessionFactory.getData().currentUser.user;
-                $scope.balance = SessionFactory.getData().balance;
+
+                $http.get('users/balance/' + SessionFactory.getData().currentUser.user.id)
+                    .then(function (response) {
+                        $scope.balance = response.data;
+                        SessionFactory.addData('balance',response.data);
+
+                    }).catch(function (e) {
+                    console.log('error',e);
+                });
+
+
                 $scope.show_nav = true;
                 $scope.changePage = function(){
                     $location.path("/exchangeNow");
