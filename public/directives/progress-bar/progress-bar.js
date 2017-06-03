dcb9f7d32b091c1395ae51792aaf9cd182120454
Idan Belassen <idanbelah@gmail.com>
2017-06-04 01:11:05 +0300

    bug fixes and more

--- public/directives/progress-bar/progress-bar.js
+++ public/directives/progress-bar/progress-bar.js
@@ -19,4 +19,25 @@ angular.module('Helpers', []).directive("progressBar", function() {
             }
         }
     };
+}).directive("progressBarBalance", function() {
+    return {
+        restrict : "E",
+        scope: {
+            stage: '='
+        },
+        templateUrl : "/directives/progress-bar-balance/progress-bar-balance.html",
+        controller: function($scope, $element, $attrs, $location) {
+            $scope.circle1_link = "/AddToBalance";
+            $scope.circle2_link = "/AddToBalanceConfirm";
+            $scope.circle3_link = "/AddToBalanceFinal";
+
+            $scope.circle1 = 'btn-primary';
+            $scope.circle2 = $scope.stage > 1 ? 'btn-primary' :'btn-default';
+            $scope.circle3 = $scope.stage > 2 ? 'btn-primary' :'btn-default';
+
+            $scope.changePage = function(link){
+                $location.path(link);
+            }
+        }
+    };
 });
