2c5826c504413c6747e288e46515d1dcc5b7e73f
idanbelassen <idanbelah@gmail.com>
2017-06-14 09:38:35 +0300

    Merge branch 'master' of github.com:idanb/plotus
    # Conflicts:
    #	public/modules/about/views/about.html

--- routes/currency.js
+++ routes/currency.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var Transaction = require('../models/CurrencyModel');
+var Currency = require('../models/CurrencyModel');
 
 /* /transactions/:status/:userId
 
 /* GET All transactions listing. */
 router.get('/', function(req, res, next) {
-    Transaction.getAll().then(function (rows) {
+    Currency.getAll().then(function (rows) {
         res.json(rows);
     }, function(reason) {
         res.json(reason);
@@ -15,21 +15,12 @@ router.get('/', function(req, res, next) {
 
 /* GET all atm locations. */
 router.get('/atms', function(req, res, next) {
-    Transaction.getAllAtms().then(function (rows) {
+    Currency.getAllAtms().then(function (rows) {
         res.json(rows);
     }, function(reason) {
         res.json(reason);
     });
 });
-
-// /* GET any kind of transactions by User. */
-// router.get('/:status/:userId', function(req, res, next) {
-//     Transaction.getByUser(req.params.status, req.params.userId).then(function (rows) {
-//         res.json(rows);
-//     }, function(reason) {
-//         res.json(reason);
-//     });
-// });
 
 module.exports = router;
 
